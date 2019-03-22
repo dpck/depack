126125237b8097b7074e24ce55d967ef171c22d7
Anton <anton@adc.sh>
2019-03-22 05:42:53 +0300

    native snapshots

--- test/spec/closure/default.js
+++ test/spec/closure/default.js
@@ -1,4 +1,3 @@
-import SnapshotContext from 'snapshot-context'
 import { makeError } from '../../../src/lib/closure'
 
 const error = `node_modules/argufy/src/index.js:2: WARNING - Bad type annotation.
@@ -13,22 +12,17 @@ t/argufy.js:3: ERROR - variable src$$module$t$argufy is undeclared
 `
 /** @type {Object.<string, (c: SnapshotContext)>} */
 const TS = {
-  context: SnapshotContext,
-  async 'parses the warnings'({ setDir, test }) {
-    setDir('test/snapshot/error')
-    const res = makeError(1, error)
-    await test('warning.txt', res)
+  async 'parses the warnings'() {
+    return makeError(1, error)
   },
-  async 'parses error with original'({ setDir, test }) {
-    setDir('test/snapshot/error')
-    const res = makeError(1, `node_modules/preact/dist/preact.mjs:678:
+  async 'parses error with original'() {
+    return makeError(1, `node_modules/preact/dist/preact.mjs:678:
 Originally at:
 node_modules/preact/src/vdom/component.js:287: WARNING - dangerous use of the global this object
                 component.nextBase = base;
                 ^^^^
 
 t/argufy.js:3: ERROR - variable src$$module$t$argufy is undeclared`)
-    await test('warning-original.txt', res)
   },
 }
 
