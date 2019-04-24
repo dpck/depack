82166d435f732f21a0a428d8adf58f4be2b8ad4e
Anton <anton@adc.sh>
2019-04-24 16:41:59 +0300

    args + library

--- src/bin/depack.js
+++ src/bin/depack.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-import { _src, _output, _version, _noWarnings, _compile, _argv, _level, _language_in, _language_out, _temp, _advanced, _noStrict, _verbose, _help, _noSourceMaps, _prettyPrint, _preact, _debug, _iife } from './get-args'
-import { GOOGLE_CLOSURE_COMPILER, run, Bundle, Compile, getOptions, getCompilerVersion, getOutput } from '@depack/depack'
+import { _source, _output, _version, _noWarnings, _compile, _argv, _level, _language_in, _language_out, _temp, _advanced, _noStrict, _verbose, _help, _noSourcemap, _prettyPrint, _preact, _debug, _iife, _library } from './get-args'
+import { GOOGLE_CLOSURE_COMPILER, run, Bundle, Compile, getOptions, getCompilerVersion, getOutput } from '@depack/depack/src'
 import resolveDependency from 'resolve-dependency'
 import getUsage from './usage'
-const { version } = require('../../package.json')
 
 if (_help) {
   console.log(getUsage())
@@ -14,6 +13,7 @@ if (_help) {
   try {
     const compilerVersion = await getCompilerVersion()
     if (_version) {
+      const version = require('../../package.json')['version']
       console.log('Depack version: %s', version)
       const res = await run([
         ...getOptions({ compiler: GOOGLE_CLOSURE_COMPILER }),
@@ -21,7 +21,7 @@ if (_help) {
       console.log(res)
       return
     }
-    const { path: src } = await resolveDependency(_src)
+    const { path: src } = await resolveDependency(_source)
     const output = _output ? getOutput(_output, src) : null
     let languageOut = _language_out
     if (!_language_out && _compile) {
@@ -29,18 +29,19 @@ if (_help) {
     }
     const options = getOptions({
       compiler: GOOGLE_CLOSURE_COMPILER,
-      output, level: _level, languageIn: _language_in, languageOut, argv: _argv, advanced: _advanced, sourceMap: !!_output && !_noSourceMaps, prettyPrint: _prettyPrint, noWarnings: _noWarnings, debug: _debug, iife: _iife,
+      output, level: _level, languageIn: _language_in, languageOut, argv: _argv, advanced: _advanced, sourceMap: !!_output && !_noSourcemap, prettyPrint: _prettyPrint, noWarnings: _noWarnings, debug: _debug, iife: _iife,
     })
     const runOptions = {
       compilerVersion, output,
-      noSourceMap: _noSourceMaps || _debug, debug: _debug,
+      noSourceMap: _noSourcemap || _debug, debug: _debug,
     }
-    if (_compile) {
+    if (_compile || _library) {
       return await Compile({
         src,
         noStrict: _noStrict,
         verbose: _verbose,
         debug: _debug,
+        library: _library,
       }, runOptions, options)
     }
     await Bundle({
--- src/bin/get-args.js
+++ src/bin/get-args.js
@@ -1,135 +1,198 @@
 import argufy from 'argufy'
 
-const args = argufy({
-  'src': { command: true },
-  'advanced': { short: 'a', boolean: true },
-  'help': { short: 'h', boolean: true },
-  'output': { short: 'o' },
-  'compile': { short: 'c', boolean: true },
-  'version': { short: 'v', boolean: true },
-  'no-warnings': { short: 'w', boolean: true },
-  'level': { short: 'l' },
-  'language_in': { short: 'I' },
-  'iife': { short: 'i', boolean: true },
-  'language_out': { short: 'O' },
-  'node': { short: 'n', boolean: true },
-  'temp': { },
-  'just-temp': { short: 'T', boolean: true },
-  'no-strict': { short: 's', boolean: true },
-  'verbose': { short: 'V', boolean: true },
-  'no-sourcemap': { short: 'S', boolean: true },
-  'pretty-print': { short: 'p', boolean: true },
-  'preact': { short: 'H', boolean: true },
-  'debug': { short: 'd' },
-})
+export const argsConfig = {
+  'source': {
+    description: 'The source entry to build.',
+    command: true,
+  },
+  'output': {
+    description: 'Where to save the output.\nPrints to `stdout` when not passed.',
+    short: 'o',
+  },
+  'compile': {
+    description: 'Set the _Depack_ mode to "compile" to create a Node.JS binray.',
+    boolean: true,
+    short: 'c',
+  },
+  'preact': {
+    description: 'Add the `import { h } from "preact"` to JSX files automatically.',
+    boolean: true,
+    short: 'H',
+  },
+  'temp': {
+    description: 'The path to the temp directory used to pre-transpile JSX files.',
+    default: 'depack-temp',
+  },
+  'library': {
+    description: 'Set the _Depack_ mode to "library" to create a Node.JS library.',
+    boolean: true,
+    short: 'l',
+  },
+  'iife': {
+    description: 'Add the IIFE flag to prevent global conflicts\n(OK Google prevent world conflict).',
+    boolean: true,
+    short: 'i',
+  },
+  'debug': {
+    description: 'The location of the file where to save sources after\neach pass.',
+    short: 'd',
+  },
+  'pretty-print': {
+    description: 'Whether to apply the `--formatting=PRETTY_PRINT` flag.',
+    boolean: true,
+    short: 'p',
+  },
+  'no-sourcemap': {
+    description: 'Disable source maps.',
+    boolean: true,
+    short: 'S',
+  },
+  'no-strict': {
+    description: 'Whether to remove the `"use strict"` from the output.',
+    boolean: true,
+    short: 's',
+  },
+  'verbose': {
+    description: 'Print the exact command.',
+    boolean: true,
+    short: 'V',
+  },
+  'language_in': {
+    description: 'The language of the input sources, years also accepted.',
+    short: 'I',
+  },
+  'language_out': {
+    description: 'The language spec of the output, years accepted.',
+    short: 'O',
+  },
+  'level': {
+    description: 'The compilation level. Options:\nBUNDLE, WHITESPACE_ONLY, SIMPLE (default), ADVANCED.',
+    short: 'lvl',
+  },
+  'advanced': {
+    description: 'Whether to enable advanced optimisation.',
+    boolean: true,
+    short: 'a',
+  },
+  'no-warnings': {
+    description: 'Do not print compiler\'s warnings by adding the\n`--warning_level QUIET` flag.',
+    boolean: true,
+    short: 'w',
+  },
+  'version': {
+    description: 'Shows the current _Depack_ and _GCC_ versions.',
+    boolean: true,
+    short: 'v',
+  },
+  'help': {
+    description: 'Prints the usage information.',
+    boolean: true,
+    short: 'h',
+  },
+}
+const args = argufy(argsConfig)
 
 /**
- * The source to build.
- * @type {string}
+ * The source entry to build.
  */
-export const _src = /** @type {string} */ (args['src'])
-/**
- * Whether to apply `--formatting=PRETTY_PRINT` flag.
- * @type {boolean}
- */
-export const _prettyPrint = /** @type {boolean} */ (args['pretty-print'])
-/**
- * Whether to print help and exit.
- * @type {boolean}
- */
-const _help = /** @type {boolean} */ (args['help'])
-/**
- * Disable source maps.
- * @type {boolean}
- */
-export const _noSourceMaps = /** @type {boolean} */ (args['no-sourcemap'])
-/**
- * Whether to remove the `'use strict'` from the output.
- * @type {boolean}
- */
-export const _noStrict = /** @type {boolean} */ (args['no-strict'])
-/**
- * Print the exact command.
- * @type {boolean}
- */
-export const _verbose = /** @type {boolean} */ (args['verbose'])
-/**
- * The path to the temp directory that will be created to make bundles.
- * @type {string}
- */
-export const _temp = /** @type {string} */ (args['temp'])
-// /**
-//  * Sets `module_resolution` to `NODE`.
-//  * @type {boolean}
-//  */
-// export const _node = args['node']
-/**
- * Sets the language spec to which input sources should conform.
- * @type {string}
- */
-export const _language_in = /** @type {string} */ (args['language_in'])
-/**
- * Sets the language spec to which output should conform.
- * @type {string}
- */
-export const _language_out = /** @type {string} */ (args['language_out'])
-/**
- * The compilation level.
- * @type {string}
- */
-export const _level = /** @type {string} */ (args['level'])
-/**
- * Any additional arguments.
- * @type {Array<string>}
- */
-export const _argv = /** @type {Array<string>} */ (args._argv)
-/**
- * Run the compiler with the manually passed arguments
- * @type {boolean}
- */
-export const _compile = /** @type {boolean} */ (args['compile'])
-/**
- * Do not print compiler's warnings by adding `--warning_level QUIET`
- * @type {boolean}
- */
-const _noWarnings = /** @type {boolean} */ (args['no-warnings'])
+export const _source = /** @type {string} */ (args['source'])
 
 /**
- * The version.
- * @type {boolean}
- */
-export const _version = /** @type {boolean} */ (args['version'])
-/**
- * Whether to enable advanced optimisation.
- * @type {boolean}
- */
-export const _advanced = /** @type {boolean} */ (args['advanced'])
-/**
- * The path where to save the built file.
- * @type {string}
+ * Where to save the output.
+    Prints to `stdout` when not passed.
  */
 export const _output = /** @type {string} */ (args['output'])
-/**
- * Whether to add the Preact's pragma.
- * @type {boolean}
- */
-export const _preact = /** @type {boolean} */ (args['preact'])
-/**
- * Debug by printing each pass to the file
- * @type {string}
- */
-export const _debug = /** @type {string} */ (args['debug'])
 
 /**
- * Add the IIFE flag to prevent global conflicts (OK google prevent world conflict)
- * @type {boolean}
+ * Set the _Depack_ mode to "compile" to create a Node.JS binray.
+ */
+export const _compile = /** @type {boolean} */ (args['compile'])
+
+/**
+ * Add the `import { h } from "preact"` to JSX files automatically.
+ */
+export const _preact = /** @type {boolean} */ (args['preact'])
+
+/**
+ * The path to the temp directory used to pre-transpile JSX files. Default `depack-temp`.
+ */
+export const _temp = /** @type {string} */ (args['temp']) || 'depack-temp'
+
+/**
+ * Set the _Depack_ mode to "library" to create a Node.JS library.
+ */
+export const _library = /** @type {boolean} */ (args['library'])
+
+/**
+ * Add the IIFE flag to prevent global conflicts
+    (OK Google prevent world conflict).
  */
 export const _iife = /** @type {boolean} */ (args['iife'])
 
 /**
- * Only generate the temp directory that would be used as input.
- * @type {boolean}
+ * The location of the file where to save sources after
+    each pass.
  */
-export const _justTemp = /** @type {boolean} */ (args['just-temp'])
+export const _debug = /** @type {string} */ (args['debug'])
 
-export { _noWarnings, _help }
+/**
+ * Whether to apply the `--formatting=PRETTY_PRINT` flag.
+ */
+export const _prettyPrint = /** @type {boolean} */ (args['pretty-print'])
+
+/**
+ * Disable source maps.
+ */
+export const _noSourcemap = /** @type {boolean} */ (args['no-sourcemap'])
+
+/**
+ * Whether to remove the `"use strict"` from the output.
+ */
+export const _noStrict = /** @type {boolean} */ (args['no-strict'])
+
+/**
+ * Print the exact command.
+ */
+export const _verbose = /** @type {boolean} */ (args['verbose'])
+
+/**
+ * The language of the input sources, years also accepted.
+ */
+export const _language_in = /** @type {string} */ (args['language_in'])
+
+/**
+ * The language spec of the output, years accepted.
+ */
+export const _language_out = /** @type {string} */ (args['language_out'])
+
+/**
+ * The compilation level. Options:
+    BUNDLE, WHITESPACE_ONLY, SIMPLE (default), ADVANCED.
+ */
+export const _level = /** @type {string} */ (args['level'])
+
+/**
+ * Whether to enable advanced optimisation.
+ */
+export const _advanced = /** @type {boolean} */ (args['advanced'])
+
+/**
+ * Do not print compiler's warnings by adding the
+    `--warning_level QUIET` flag.
+ */
+export const _noWarnings = /** @type {boolean} */ (args['no-warnings'])
+
+/**
+ * Shows the current _Depack_ and _GCC_ versions.
+ */
+export const _version = /** @type {boolean} */ (args['version'])
+
+/**
+ * Prints the usage information.
+ */
+export const _help = /** @type {boolean} */ (args['help'])
+
+/**
+ * The additional arguments passed to the program.
+ */
+export const _argv = /** @type {!Array<string>} */ (args._argv)
