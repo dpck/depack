2a635f62a272586505c65ce849d65ca46fb2c0a7
Anton <anton@adc.sh>
2019-07-16 23:49:35 +0300

    comon

--- example/commonjs/common-js2.js
+++
@@ -1,7 +0,0 @@
-// common-js
-module.exports = () => {
-  console.log('default common js export2')
-}
-module.exports['named'] = () => {
-  console.log('named common js export2')
-}
--- example/commonjs/index.js
+++
@@ -1,5 +0,0 @@
-// ecma
-import commonJs from './common-js'
-
-console.log('requiring a common js from ecma:')
-console.log(commonJs)
