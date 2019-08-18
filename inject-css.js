85f5ad856d2732ad0755675430f9eb61ae523ba4
Anton <anton@adc.sh>
2019-08-18 14:19:10 +0300

    inject css fix

--- inject-css.js
+++ inject-css.js
@@ -1,6 +1,5 @@
 /* eslint-env browser */
 export default function __$styleInject(css = '') {
-  try { if (!document) return } catch (err) { return }
   const head = document.head
   const style = document.createElement('style')
   style.type = 'text/css'
